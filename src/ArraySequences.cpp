1c3219d60bd154c0cd7d5216304ed1abc40387a7
chandu54 <chandu54@missionrnd.com>
2016-04-02 20:57:55 +0530

    Submit Commit

--- src/ArraySequences.cpp
+++ src/ArraySequences.cpp
@@ -32,6 +32,64 @@ Difficulty : Medium
 #include <math.h>
 
 int * find_sequences(int *arr, int len){
+	if (arr == NULL&&len<0)
+		return NULL;
+	int *resultArr, i = 0, j = 0, k, diff, ratio;
+	resultArr = (int*)malloc(sizeof(int) * 6);
+	for (i = 0; i < 6; i++)
+		resultArr[i] = 0;
+	i = 0;
+	diff = arr[1] - arr[0];
+	ratio = arr[1] / arr[0];
+	if ((arr[i + 1] - arr[i] == diff) && (arr[i + 2] - arr[i + 1] == diff))
+	{
+		resultArr[0] = i;
+		while (arr[i + 1] - arr[i] == diff)
+		{
+			i++;
+		}
+		resultArr[1] = i;
+		j = i + 1;
+	}
+	else if ((arr[i + 1] / arr[i] == ratio) && (arr[i + 2] / arr[i + 1] == ratio))
+	{
+		resultArr[4] = i;
+		while (arr[i + 1] / arr[i] == ratio)
+		{
+			i++;
+		}
+		resultArr[5] = i;
+		j = i;
+	}
+	j = i + 1;
+	diff = arr[j + 1] - arr[j];
+	ratio = arr[j + 1] / arr[j];
+	if ((arr[j + 1] - arr[j] == diff) && (arr[j + 2] - arr[j + 1] == diff))
+	{
+		if (resultArr[0] != 0)
+			resultArr[0] = j;
+		else
+			resultArr[2] = j;
+		while (arr[j + 1] - arr[j] == diff)
+		{
+			j++;
+		}
+		if (resultArr[0] != 0)
+			resultArr[1] = j;
+		else
+			resultArr[3] = j;
+
+	}
+	k = j + 1;
+	ratio = arr[k + 1] / arr[k];
+	if (arr[k] / arr[k - 1] == ratio)
+	{
+		k = k - 1;
+	}
+	resultArr[4] = k;
+	while (arr[k + 1] / arr[k] == ratio)
+		k++;
+	resultArr[5] = k;
 	//Return final array which has 6indexes [AP1_S,AP1_E,AP2_S,AP2_E,GP1_S,GP2_E]
-	return NULL;
+	return resultArr;
 }
